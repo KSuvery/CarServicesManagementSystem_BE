6938cc29e88b201e8e8e5711d5d964e269460a38
MinhNhatTyson <minhnhat0132@gmail.com>
2025-08-08 08:58:52 +0700

    Show part needed for one Service

---
+++ CarServ.Repository/Repositories/DTO/Booking A Service/PartDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServ.Repository.Repositories.DTO.Booking_A_Service
+{
+    internal class PartDTO
+    {
+    }
+}
