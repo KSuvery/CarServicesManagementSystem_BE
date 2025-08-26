9654289ea34d8de91acb707cccf80f833db3474e
KSuvery <luonglkse171576@fpt.edu.vn>
2025-08-26 14:57:33 +0700

    [MODIFIED] Update DB: Vehicle.cs [LastService] and [NextService] changed to DateTime

--- CarServ.Domain/Entities/Vehicle.cs
+++ CarServ.Domain/Entities/Vehicle.cs
@@ -23,9 +23,9 @@ public partial class Vehicle
 
     public string Status { get; set; }
 
-    public string LastService { get; set; }
+    public DateTime? LastService { get; set; }
 
-    public string NextService { get; set; }
+    public DateTime? NextService { get; set; }
 
     public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
 
