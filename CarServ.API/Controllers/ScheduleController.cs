64a91390c00ec07c7927da529a2d9954a2f87cbd
MinhNhatTyson <minhnhat0132@gmail.com>
2025-09-22 14:43:06 +0700

    Adding controller and config

---
+++ CarServ.API/Controllers/ScheduleController.cs
@@ -0,0 +1,19 @@
+﻿using CarServ.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarServ.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ScheduleController : Controller
+    {
+        private readonly IScheduleService _scheduleService;
+
+        public ScheduleController(IScheduleService services)
+        {
+            _scheduleService = services;
+        }
+    }
+}
